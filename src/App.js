cb7aa528b64ce5ea02a6df403aff3474f86b2d31
hinsjain <hinsjain8983@gmail.com>
2021-11-19 17:47:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import db from './firebase';
+import firebase from 'firebase/compat/app'
+import Todo from './Todo';
 
 function App() {
+
+  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("")
+
+  useEffect( () => {
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot( snapshot => {
+      setTodos(snapshot.docs.map(doc => doc.data().todo))
+    })
+  }, [] )
+  
+  const addTodo = (event) => {
+    event.preventDefault(); //don't refresh the page after adding todo
+
+    db.collection('todos').add({
+      todo: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+
+    setInput("") //set input to blank after adding todo
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <FormControl>
+          <InputLabel htmlFor="my-input">Add your task</InputLabel>
+          <Input value={input} onChange={ event => setInput(event.target.value) } />
+        </FormControl>
+        
+        <Button color="primary" disabled={!input} variant="contained" type="submit" onClick={addTodo}>Add New</Button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <Todo text={todo} />
+        ))}
+      </ul>
     </div>
   );
 }
